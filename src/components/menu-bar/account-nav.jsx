4c4136a7de25ba0e27985963f7f14784b9adbd91
PerfectMYGHY <916881890@qq.com>
2024-08-31 18:26:11 +0800

    更新

--- src/components/menu-bar/account-nav.jsx
+++ src/components/menu-bar/account-nav.jsx
@@ -55,64 +55,64 @@ const AccountNavComponent = ({
                     draggable={false}
                 />
             </div>
+            <MenuBarMenu
+                className={menuBarMenuClassName}
+                open={isOpen}
+                // note: the Rtl styles are switched here, because this menu is justified
+                // opposite all the others
+                place={isRtl ? 'right' : 'left'}
+                onRequestClose={onClose}
+            >
+                <MenuItemContainer href={profileUrl}>
+                    <FormattedMessage
+                        defaultMessage="Profile"
+                        description="Text to link to my user profile, in the account navigation menu"
+                        id="gui.accountMenu.profile"
+                    />
+                </MenuItemContainer>
+                <MenuItemContainer href="/mystuff/">
+                    <FormattedMessage
+                        defaultMessage="My Stuff"
+                        description="Text to link to list of my projects, in the account navigation menu"
+                        id="gui.accountMenu.myStuff"
+                    />
+                </MenuItemContainer>
+                {isEducator ? (
+                    <MenuItemContainer href="/educators/classes/">
+                        <FormattedMessage
+                            defaultMessage="My Classes"
+                            description="Text to link to my classes (if I am a teacher), in the account navigation menu"
+                            id="gui.accountMenu.myClasses"
+                        />
+                    </MenuItemContainer>
+                ) : null}
+                {isStudent ? (
+                    <MenuItemContainer href={`/classes/${classroomId}/`}>
+                        <FormattedMessage
+                            defaultMessage="My Class"
+                            description="Text to link to my class (if I am a student), in the account navigation menu"
+                            id="gui.accountMenu.myClass"
+                        />
+                    </MenuItemContainer>
+                ) : null}
+                <MenuItemContainer href="/accounts/settings/">
+                    <FormattedMessage
+                        defaultMessage="Account settings"
+                        description="Text to link to my account settings, in the account navigation menu"
+                        id="gui.accountMenu.accountSettings"
+                    />
+                </MenuItemContainer>
+                <MenuSection>
+                    <MenuItemContainer onClick={onLogOut}>
+                        <FormattedMessage
+                            defaultMessage="Sign out"
+                            description="Text to link to sign out, in the account navigation menu"
+                            id="gui.accountMenu.signOut"
+                        />
+                    </MenuItemContainer>
+                </MenuSection>
+            </MenuBarMenu>
         </MenuLabel>
-        <MenuBarMenu
-            className={menuBarMenuClassName}
-            open={isOpen}
-            // note: the Rtl styles are switched here, because this menu is justified
-            // opposite all the others
-            place={isRtl ? 'right' : 'left'}
-            onRequestClose={onClose}
-        >
-            <MenuItemContainer href={profileUrl}>
-                <FormattedMessage
-                    defaultMessage="Profile"
-                    description="Text to link to my user profile, in the account navigation menu"
-                    id="gui.accountMenu.profile"
-                />
-            </MenuItemContainer>
-            <MenuItemContainer href="/mystuff/">
-                <FormattedMessage
-                    defaultMessage="My Stuff"
-                    description="Text to link to list of my projects, in the account navigation menu"
-                    id="gui.accountMenu.myStuff"
-                />
-            </MenuItemContainer>
-            {isEducator ? (
-                <MenuItemContainer href="/educators/classes/">
-                    <FormattedMessage
-                        defaultMessage="My Classes"
-                        description="Text to link to my classes (if I am a teacher), in the account navigation menu"
-                        id="gui.accountMenu.myClasses"
-                    />
-                </MenuItemContainer>
-            ) : null}
-            {isStudent ? (
-                <MenuItemContainer href={`/classes/${classroomId}/`}>
-                    <FormattedMessage
-                        defaultMessage="My Class"
-                        description="Text to link to my class (if I am a student), in the account navigation menu"
-                        id="gui.accountMenu.myClass"
-                    />
-                </MenuItemContainer>
-            ) : null}
-            <MenuItemContainer href="/accounts/settings/">
-                <FormattedMessage
-                    defaultMessage="Account settings"
-                    description="Text to link to my account settings, in the account navigation menu"
-                    id="gui.accountMenu.accountSettings"
-                />
-            </MenuItemContainer>
-            <MenuSection>
-                <MenuItemContainer onClick={onLogOut}>
-                    <FormattedMessage
-                        defaultMessage="Sign out"
-                        description="Text to link to sign out, in the account navigation menu"
-                        id="gui.accountMenu.signOut"
-                    />
-                </MenuItemContainer>
-            </MenuSection>
-        </MenuBarMenu>
     </React.Fragment>
 );
 
