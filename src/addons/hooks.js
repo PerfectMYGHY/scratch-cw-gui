fd2e87aebf6e7db370fa405a1c17d4f182b9e5b0
PerfectMYGHY <916881890@qq.com>
2026-07-21 23:00:27 +0800

    chore: 删除无用注释

--- src/addons/hooks.js
+++ src/addons/hooks.js
@@ -6,21 +6,5 @@ const AddonHooks = {
     blocklyCallbacks: [],
     recolorCallbacks: []
 };
-//const AddonHooks = {
-//    appStateReducer: () => { },
-//    _appStateStore: null,
-//    set appStateStore(value) {
-//        console.error('属性 prop 被设置为：', value);
-//        console.log(new Error("abc").stack);
-//        this._appStateStore = value;
-//    },
-//    get appStateStore() {
-//        return this._appStateStore;
-//    },
-//    blockly: null,
-//    blocklyWorkspace: null,
-//    blocklyCallbacks: [],
-//    recolorCallbacks: []
-//};
 
 export default AddonHooks;
